70ba471785e76d5f8da832b9d878dec7f81ee7bf
Riadh laroui <larouiriadh@gmail.com>
2025-05-12 23:46:59 -0800

    fix: update PocketBase initialization URL to use environment variable for better configuration management

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,22 +1,21 @@
 // app/(auth)/layout.tsx
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "sonner";
 
 export default function AuthLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </main>
-  );
+	return (
+		<main className="min-h-screen flex items-center justify-center">
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				{children}
+			</ThemeProvider>
+		</main>
+	);
 }
