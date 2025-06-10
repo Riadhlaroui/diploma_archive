60ffaac6d8af5045e297a9c11f1783f07951417b
Riadh laroui <larouiriadh@gmail.com>
2025-06-10 15:29:08 -0800

    feat: simplify ThemeProvider usage in AuthLayout component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,12 +8,7 @@ export default function AuthLayout({
 }) {
 	return (
 		<main className="min-h-screen flex items-center justify-center">
-			<ThemeProvider
-				attribute="class"
-				defaultTheme="system"
-				enableSystem
-				disableTransitionOnChange
-			>
+			<ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
 				{children}
 			</ThemeProvider>
 		</main>
