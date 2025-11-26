aac36fb44f97d755617dac608006efe5417a2b30
Riadh <larouiriadh@gmail.com>
2025-11-26 18:17:17 +0100

    Update the dependencies and fix pocketbase auto cancellation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 	const router = useRouter();
 
 	return (
 		<div className="w-full flex flex-col items-center justify-center gap-2 transition-colors duration-300">
-			This is the default home page
+			This is the home page
 			<button
 				onClick={() => router.push("/sign-in")}
 				className="outline outline-dashed p-2 mt-2 hover:cursor-pointer"
