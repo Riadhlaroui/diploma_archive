8eb902982b543f8aba8190fcd7ca49ce8dd6642e
Riadh <larouiriadh@gmail.com>
2026-02-26 23:12:22 +0100

    feat: Add IBM_Plex_Sans_Arabic font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@
 module.exports = {
 	content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-inter)", "sans-serif"],
+				cairo: ["var(--font-cairo)", "sans-serif"],
+			},
+		},
 	},
 	plugins: [
 		// ...other plugins
